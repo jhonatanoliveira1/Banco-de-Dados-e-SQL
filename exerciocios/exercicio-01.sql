f7bf38afd52b56e2d22ee6f13106590451566ea8
Jhonatan de Oliveira <jhonatan.d.oliveira1@outlook.com>
2022-02-26 12:21:39 -0300

    README - EXERCICIO - 01

--- exerciocios/exercicio-01.sql
+++ exerciocios/exercicio-01.sql
@@ -1,3 +1,5 @@
+/* Resolvendo o exercício */
+
 CREATE DATABASE LIVRARIA;
 
 SHOW DATABASE;
@@ -15,6 +17,7 @@ CREATE TABLE LIVROS (
 	ANO INT(4)
 );
 
+/*DESCREVENDO AS COLUNAS*/
 INSERT INTO LIVROS(LIVRO,AUTOR,SEXO,PAGINAS,EDITORA,VALOR,UF,ANO) VALUES('Cavaleiro Real','Ana Claudia','F',465,'Atlas',49.9,'RJ',2009),
 																																				('SQL para leigos','João Nunes','M',450,'Addison',98,'SP',2018),
 																																				('Receitas Caseiras','Celia Tavares','F',210,'Atlas',45,'RJ',2008),
@@ -26,6 +29,17 @@ INSERT INTO LIVROS(LIVRO,AUTOR,SEXO,PAGINAS,EDITORA,VALOR,UF,ANO) VALUES('Cavale
 																																				('Copas Inesqueciveis','Marco Alcantara','M',200,'Larson',130.98,'RS',2018),
 																																				('O poder da mente','Clara Mafra','F',120,'Addison',56.58,'SP',2017);
 
+/*OMITINDO AS COLUNAS*/
+INSERT INTO LIVROS VALUES('Cavaleiro Real','Ana Claudia','F',465,'Atlas',49.9,'RJ',2009);
+INSERT INTO LIVROS VALUES('SQL para leigos','João Nunes','M',450,'Addison',98,'SP',2018);
+INSERT INTO LIVROS VALUES('Receitas Caseiras','Celia Tavares','F',210,'Atlas',45,'RJ',2008);
+INSERT INTO LIVROS VALUES('Pessoas Efetivas','Eduardo Santos','M',390,'Beta',78,'RJ',2018);
+INSERT INTO LIVROS VALUES('Habitos Saudáveis','Eduardo Santos','M',630,'Beta',150,'RJ',2019);
+INSERT INTO LIVROS VALUES('A Casa Marrom','Hermes Macedo','M',250,'Bubba',60,'MG',2016);
+INSERT INTO LIVROS VALUES('Estacio Querido','Geraldo Francisco','M',310,'Insignia',100,'ES',2015);
+INSERT INTO LIVROS VALUES('Pra sempre amigas','Leda Silva','F',510,'Insignia',78,'ES',2011);
+INSERT INTO LIVROS VALUES('Copas Inesqueciveis','Marco Alcantara','M',200,'Larson',130,'RS',2018);
+INSERT INTO LIVROS VALUES('O poder da mente','Clara Mafra','F',120,'Continental',56,'SP',2017);
 
 /* 1 – Trazer todos os dados */
 
@@ -35,20 +49,56 @@ SELECT LIVRO,AUTOR,SEXO,PAGINAS,EDITORA,VALOR,UF,ANO FROM LIVROS;
 
 /* 2 – Trazer o nome do livro e o nome da editora */
 
-SELECT LIVRO, EDITORA FROM LIVROS;
+SELECT LIVRO, EDITORA 
+FROM LIVROS;
 
 /* 3 – Trazer o nome do livro e a UF dos livros publicados por autores do sexo masculino. */
 
-SELECT LIVRO, UF FROM LIVROS WHERE SEXO = 'M';
+SELECT LIVRO, UF 
+FROM LIVROS 
+WHERE SEXO = 'M';
+
+SELECT AUTOR, SEXO, LIVRO, UF
+FROM LIVROS
+WHERE SEXO = 'M';
+
 
 /* 4 - Trazer o nome do livro e o número de páginas dos livros publicados por autores do sexo feminino. */
 
-SELECT LIVRO, PAGINAS FROM LIVROS WHERE SEXO = 'F';
+SELECT LIVRO, PAGINAS 
+FROM LIVROS 
+WHERE SEXO = 'F';
+
+SELECT AUTOR,  LIVRO, PAGINAS
+FROM LIVROS
+WHERE SEXO = 'F';
+
 
 /* 5 – Trazer os valores dos livros das editoras de São Paulo. */
 
-SELECT LIVRO, VALOR FROM LIVROS WHERE UF = 'SP';
+SELECT LIVRO, VALOR 
+FROM LIVROS 
+WHERE UF = 'SP';
+
+SELECT UF, LIVRO, VALOR
+FROM LIVROS
+WHERE UF = 'SP';
 
 /* 6 – Trazer os dados dos autores do sexo masculino que tiveram livros publicados por São Paulo ou Rio de Janeiro (Questão Desafio). */
 
-SELECT AUTOR, SEXO FROM LIVROS WHERE SEXO = 'M' AND UF = 'SP' OR UF = 'RJ';
+SELECT AUTOR, SEXO, UF
+FROM LIVROS 
+WHERE SEXO = 'M' 
+AND UF = 'SP' 
+OR UF = 'RJ';
+
+SELECT AUTOR, SEXO, UF
+FROM LIVROS
+WHERE 
+SEXO = 'M' AND (UF = 'SP' OR UF = 'RJ');
+
+SELECT AUTOR, SEXO, UF
+FROM LIVROS
+WHERE SEXO = 'M'
+AND UF = 'SP'
+AND paginas > 100;
